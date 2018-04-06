1c4bd0c18c47546b902a100dc22034cec6c762ba
Andrew Start <start.andrew@gmail.com>
2018-04-06 09:57:33 -0400

    Finished Wait tests.

--- tests/wait.test.ts
+++ tests/wait.test.ts
@@ -3,7 +3,7 @@ import {Resolver, TestSession} from './utils';
 import assert = require('assert');
 import sinon = require('sinon');
 
-describe.only(`Wait State`, function() {
+describe(`Wait State`, function() {
 	beforeEach(function() {
 		this.sandbox = sinon.createSandbox({useFakeTimers:true});
 	});
@@ -37,12 +37,57 @@ describe.only(`Wait State`, function() {
 		const last = new Resolver(`Last`, `output`, 42);
 		sm.addTransition(``, first, last);
 		sm.addTransition(``, last);
-		const result = sm.run({})
+		sm.run({})
 		.then((result) => {
-			throw new Error('Should not have resolved');
+			throw new Error(`Should not have resolved`);
 		});
 		//now tick the clock
 		this.sandbox.clock.tick(28);
+		//clean up timers so that we can let promises tick through
+		this.sandbox.clock.restore();
+		await new Promise(resolve => setTimeout(resolve, 10));
+		assert(last.onEntrySpy.notCalled, `Wait should not have resolved`);
+	});
+	
+	it(`Wait passes through output from previous state`, function() {
+		//restore clock here because it interferes with promise resolution
+		this.sandbox.clock.restore();
+		
+		const sm = new StateMachine<TestSession, number>();
+		const first = new Resolver(`First`, `trans`, `foo`);
+		sm.addTransition(null, null, first);
+		const wait = new Wait(`Wait`, 30);
+		sm.addTransition(``, first, wait);
+		const last = new Resolver(`Last`, `output`, 42);
+		sm.addTransition(``, wait, last);
+		sm.addTransition(``, last);
+		const result = sm.run({})
+		.then((result) => {
+			assert(last.onEntrySpy.calledOnce, `Last state should have been entered`);
+			assert.equal(last.onEntrySpy.getCall(0).args[2], `foo`, `Wait should have passed input through as output`);
+			assert.equal(last.onEntrySpy.getCall(0).args[3], `trans`, `Wait should have reused transition`);
+		});
 		return result;
+	});
+	
+	it(`Cancelled Wait cleans up timer`, function() {
+		const setSpy = sinon.spy(global, `setTimeout`);
+		const clearSpy = sinon.spy(global, `clearTimeout`);
+		
+		const sm = new StateMachine<TestSession, number>();
+		const first = new Wait(`First`, 30);
+		sm.addTransition(null, null, first);
+		const last = new Resolver(`Last`, `output`, 42);
+		sm.addTransition(``, first, last);
+		sm.addTransition(``, last);
+		const session = {};
+		sm.run(session)
+		.then((result) => {
+			throw new Error(`Should not have resolved`);
+		});
+		sm.stop(session);
+		assert(setSpy.calledOnce, `setTimeout should have been called once`);
+		assert(clearSpy.calledOnce, `clearTimeout should have been called once`);
+		assert(clearSpy.calledWith(setSpy.getCall(0).returnValue), `clearTimeout should have been called with the value from setTimeout`);
 	});
 });
